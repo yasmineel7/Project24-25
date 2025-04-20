daa6917c7e1718e1fc95a68dd671d10ac595bea2
yasmineel7 <6298686@edu.vaniercollege.qc.ca>
2025-04-19 21:33:56 -0400

    Add files via upload

---
+++ Administrator.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.LinkedList;
+
+public class Administrator extends User{
+
+
+    public Administrator(String name, int id) {
+        super(name, id);
+    }
+
+    private static LinkedList<Student> readTranscript(String path) {
+        //TODO: read the transcript odf the students
+    }
+}
